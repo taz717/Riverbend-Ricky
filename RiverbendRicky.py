32b351e4d52bfbae16772eee5c34a51563a1f7c2
Moataz Khallaf <48331207+taz717@users.noreply.github.com>
2020-11-14 01:42:28 -0700

    tried to make blackjack, didn't work, made diceroll

--- RiverbendRicky.py
+++ RiverbendRicky.py
@@ -27,7 +27,7 @@ async def on_ready():
     print(f'{client.user.name} has connected to Discord! Get ready for a shit show!')
 
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='pls ')
 
 
 @bot.command(name='ape', help="this makes monkey noises!")
@@ -36,5 +36,56 @@ async def plsText(ctx):
     response = "ooh ooh ahh EEEEEEE"
     await ctx.send(response)
 
-client.run(TOKEN)
+
+@bot.command(name="dab", help="*dab*")
+async def plsDab(ctx):
+
+    response = "___Bitch___ *DAB!!!*"
+    await ctx.send(response)
+
+
+@bot.command(name='roll_dice', help='Simulates rolling dice. (# of dice, # of sides)')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
+@bot.command(name='blackjack', help='Simulates blackJack. (playername)')
+async def deal(ctx, playername):
+    values = {
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5,
+        'Six': 6,
+        'Seven': 7,
+        'Eight': 8,
+        'Nine': 9,
+        'Ten': 10,
+        'Jack': 10,
+        'Queen': 10,
+        'King': 10
+    }
+    cards = [['Ace', 'Two', 'Three', 'Four', 'Five', "Six", 'Seven', 'Eight',
+              'Nine', 'Ten', 'Jack', 'Queen', 'King'],
+             ['Spades', 'Diamonds', 'Hearts', 'Clubs']
+             ]
+
+    def arrayRandPick(array):
+        return random.randrange(0, len(array))
+
+    def generateCard(cards, values):
+        card = cards[0][arrayRandPick(
+            cards[0])], cards[1][arrayRandPick(cards[0])]
+        value = values[card[0]]
+
+        return card, value
+
+    await ctx.send(generateCard(cards, values))
+
+
+# client.run(TOKEN)
 bot.run(TOKEN)
